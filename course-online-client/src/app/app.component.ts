5e232c6f45de1b2252eed3d617847e247e603755
Chaya44099 <c0556744099@gmail.com>
2025-03-10 23:18:28 +0200

    change

--- course-online-client/src/app/app.component.ts
+++ course-online-client/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginButtonsComponent } from './components/authentication/login-buttons/login-buttons.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- course-online-client/src/app/app.routes.ts
+++ course-online-client/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginFormComponent } from './components/authentication/login-form/login-form.component';
 import { LoginButtonsComponent } from './components/authentication/login-buttons/login-buttons.component';
+import { LoginFormComponent } from './components/authentication/login-form/login-form.component';
 import { LogupFormComponent } from './components/authentication/logup-form/logup-form.component';
 import { CoursesListComponent } from './components/courses/courses-list/courses-list.component';
 import { authGuard } from './guards/auth.guard';
@@ -8,20 +8,21 @@ import { CourseDetailsComponent } from './components/courses/course-details/cour
 import { CourseEditComponent } from './components/courses/course-edit/course-edit.component';
 import { CourseAddComponent } from './components/courses/course-add/course-add.component';
 import { LessonAddComponent } from './components/courses/lessons/lesson-add/lesson-add.component';
+import { teacherGuard } from './guards/teacher.guard';
 import { LessonEditComponent } from './components/courses/lessons/lesson-edit/lesson-edit.component';
+
 
 export const routes: Routes = [
     { path: '', component: LoginButtonsComponent, pathMatch: 'full' },
     { path: 'login', component: LoginFormComponent },
     { path: 'logup', component: LogupFormComponent },
     { path: 'courseslist', component: CoursesListComponent, canActivate: [authGuard] },
-    { path: 'courses', component: CoursesListComponent },
-    { path: 'courses/details/:id', component: CourseDetailsComponent },
-    { path: 'courses/edit/:courseId', component: CourseEditComponent },
-    { path: 'courses/add', component: CourseAddComponent },
-    { path: 'lessons/add', component: LessonAddComponent },
-    { path: 'courses/lessons/edit/:courseId/:id', component: LessonEditComponent },
+    { path: 'courses', component: CoursesListComponent, canActivate: [authGuard] },
+    { path: 'courses/details/:id', component: CourseDetailsComponent, canActivate: [authGuard] },
+    { path: 'courses/edit/:courseId', component: CourseEditComponent, canActivate: [teacherGuard] },
+    { path: 'courses/add', component: CourseAddComponent, canActivate: [teacherGuard] },
+    { path: 'lessons/add', component: LessonAddComponent, canActivate: [teacherGuard] },
+    { path: 'courses/lessons/edit/:courseId/:id', component: LessonEditComponent, canActivate: [teacherGuard] }
 
 
-
-];
+ ];
